b6f536f5592d952994710a8c631dc5ef3d395884
Evan <evan.froc35@gmail.com>
2024-12-21 13:03:14 +0100

    init

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="src/style/style.css">
+    <title>Portfolio of Evan Froc</title>
+</head>
+<body>
+    <header>
+        <h2 class="titre">My Port<span style="color: #d82d47;">folio</span></h2>
+        <ul class="head-menu">
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="index.php">Compétences</a></li>
+            <li><a href="index.php">Projets</a></li>
+            <li><a href="index.php">Contact</a></li>
+        </ul>  
+        <div class="download-cv">
+            <a href="ressources/CV.pdf" download="EVAN_FROC_CV.pdf">Télécharger le CV</a>
+        </div>
+    </header>
+
+    <div class="main-page">
+        <!--BODY-->
+    </div>
+
+    <footer>
+        
+    </footer>
+</body>
+</html>
