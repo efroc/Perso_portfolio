def2e02f84cf5d0e3b2fd29949004ae09881fa15
Evan <evan.froc35@gmail.com>
2025-02-15 14:58:35 +0100

    Accueil + debut projets

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,31 @@
+function showContent(contentId) {
+    // Récupère les class="content"
+    var contents = document.querySelectorAll('.content');
+    contents.forEach(function(content) {
+        // Cache les autres
+        content.style.display = 'none';
+    });
 
+    // Afficher le id="contentId"
+    var selectedContent = document.getElementById(contentId);
+    selectedContent.style.display = 'flex';
+}
+
+
+document.getElementById("language-button").addEventListener("click", function() {
+    // Sélectionner les éléments par leur classe
+    var frenchContent = document.querySelectorAll(".fr");
+    var englishContent = document.querySelectorAll(".en");
+  
+    // Basculer la visibilité des éléments
+    if (frenchContent[0].style.display === 'none') {
+        // Afficher les éléments en français et cacher ceux en anglais
+        frenchContent.forEach(p => p.style.display = 'block');
+        englishContent.forEach(p => p.style.display = 'none');
+    } else {
+        // Afficher les éléments en anglais et cacher ceux en français
+        frenchContent.forEach(p => p.style.display = 'none');
+        englishContent.forEach(p => p.style.display = 'block');
+    }
+});
   
